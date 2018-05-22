67ceed19b503d6c1f83553b01a8c8b42450973e3
ming <ming@mingdeimac.local>
2018-05-22 10:10:15 +0800

    Initial Commit

---
+++ MyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by ming on 2018/5/22.
+//  Copyright © 2018年 ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by ming on 2018/5/22.
+//  Copyright © 2018年 ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
